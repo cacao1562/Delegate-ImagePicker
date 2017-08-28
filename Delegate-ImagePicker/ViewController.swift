c9f9a3523f5ccb366fe070086dd2cdf95972adff
hwan Yu <hwanungyu@hwanui-MacBook-Pro.local>
2017-08-28 11:09:34 +0900

    constraint 수정

--- Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/ViewController.swift
@@ -1,25 +1,67 @@
-//
-//  ViewController.swift
-//  Delegate-ImagePicker
-//
-//  Created by hwan ung Yu on 2017. 8. 16..
-//  Copyright © 2017년 hwan ung Yu. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var imgView: UIImageView!
+ 
+    @IBAction func pick(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        picker.delegate = self
+        
+        self.present(picker, animated: false)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        self.dismiss(animated: false) { () in //트레일링 클로저 마지막 매개변수 생략 (completion) picker가 완전히 닫힌후 alert창 실행
+        
+        let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: false) { () in
+            
+            let img = info[UIImagePickerControllerEditedImage] as? UIImage //편집모드, MediaType,OriginalImage,CropRect
+            self.imgView.image = img
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraPick(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        
+        picker.delegate = self
+        
+        self.present(picker, animated: false)
+        
     }
-
+    
+    @IBAction func albumPick(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .savedPhotosAlbum
+        picker.allowsEditing = true
+        
+        picker.delegate = self
+        
+        self.present(picker, animated: false)
+    }
 
 }
 
